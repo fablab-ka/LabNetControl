06d011d1738270b56dd57a366d34317776052cb9
morbidick <lukas.mohrbacher@web.de>
2015-04-06 07:09:27 +0200

    added song view

--- server/mpd.js
+++ server/mpd.js
@@ -25,15 +25,22 @@ var mpdAsync = function(command) {
 
 
 var mpdUpdateStatus = function() {
-  var data = mpdAsync("status");
-  if(data) {
+  var mpd_info = mpdAsync("status");
+  if(mpd_info) {
     musicdb.update({_id: "mpd"}, {$set: {
-      volume: parseInt(data.volume),
-      state: data.state,
-      song: data.song
+      volume: parseInt(mpd_info.volume),
+      state: mpd_info.state
+    }});
+  }
+
+  var song_info = mpdAsync("playlistinfo " + mpd_info.song);
+  if(song_info) {
+    musicdb.update({_id: "mpd"}, {$set: {
+      song: song_info
     }});
   }
 }
+
 
 mpdclient.on('ready', Meteor.bindEnvironment(function() {
   musicdb.upsert({_id: "mpd"}, {$set: {connected: true}});
